0ef1af82a5126e1b7a7df7b64b6310b0daa69dd6
jaikumarch33 <jaikumarch33@gmail.com>
2021-01-07 23:52:21 +0530

    incomplete quick sort algorithm

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -3,10 +3,18 @@
 data = [10, 80, 30, 90, 40, 50, 70]
 
 
-def quick_sort(arr):
+def quick_sort(arr, low=None, high=None):
     if len(arr) < 1:
         return data
 
-    pivot=arr[-1]
+    low = 0 - 1
+    high = len(arr)
+    pivot = arr[-1]
 
+    for i in range(high - 1):
+        if arr[i] < pivot:
+            low += 1
+            arr[i], arr[low] = arr[low], arr[i]
+
+    return data
 
